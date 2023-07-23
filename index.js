50a3c4f719cf70888d0bb7e9a7dbc5642362c665
Nitish Kumar <kumar.nit1021@gmail.com>
2023-07-24 01:35:01 +0530

    Changed

--- index.js
+++ index.js
@@ -1,140 +1,143 @@
-const express = require('express')
-const http = require('http')
-const socketio = require('socket.io')
-const cors=require('cors')
-const passport = require("passport")
-const mongoose = require("mongoose")
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const cors = require("cors");
+const passport = require("passport");
+const mongoose = require("mongoose");
 
-const { userJoin, getRoomUsers, getCurrentUser, userLeave, formateMessage} = require('./users')
+const {
+	userJoin,
+	getRoomUsers,
+	getCurrentUser,
+	userLeave,
+	formateMessage,
+} = require("./users");
 
-const { userRouter } = require('./routes/user.route')
-const { paymentRouter } = require('./routes/payment.route')
+const { userRouter } = require("./routes/user.route");
+const { paymentRouter } = require("./routes/payment.route");
 // const cors=require('cors')
-require("dotenv").config()
+require("dotenv").config();
 
 const handlebars = require("express-handlebars");
 
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
-
-app.use(cors())
-
+app.use(cors());
+app.use(express.json());
 const socketsStatus = {};
 
 const customHandlebars = handlebars.create({ layoutsDir: "./views" });
 
 // --------------------------GOAuth------------------------
 
+const { googleAuthentication } = require("./middlewares/G_OAuth.js");
+const exp = require("constants");
 
-const { googleAuthentication } = require("./middlewares/G_OAuth.js")
+app.get(
+	"/auth/google",
+	passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-
-
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login', session:false }), googleAuthentication )
-
+app.get(
+	"/auth/google/callback",
+	passport.authenticate("google", {
+		failureRedirect: "/login",
+		session: false,
+	}),
+	googleAuthentication
+);
 
 // --------------------------GOAuth------------------------
-
-
 
 app.engine("handlebars", customHandlebars.engine);
 app.set("view engine", "handlebars");
 
-app.use("/files", express.static("public")); 
+app.use("/files", express.static("public"));
+app.use("/user", userRouter);
+app.use("/payment", paymentRouter);
 
+io.on("connection", (socket) => {
+	const socketId = socket.id;
+	socketsStatus[socket.id] = {};
 
-app.use("/user",userRouter);
-app.use("/payment",paymentRouter)
+	socket.on("joinRoom", ({ username, room }) => {
+		const user = userJoin(socket.id, username, room);
+		console.log(user);
+		socket.join(user.room);
 
-io.on('connection', (socket) => {
-  const socketId = socket.id;
-  socketsStatus[socket.id] = {};
+		// Welcome message
+		socket.emit("message", `Chat AI :- Welcome to ${room}`);
 
-  socket.on('joinRoom', ({ username, room }) => {
-    const user = userJoin(socket.id, username, room)
-    console.log(user)
-    socket.join(user.room)
+		// Broadcasting other users
+		socket.broadcast
+			.to(room)
+			.emit("message", `Chat AI :- ${username} has joined the room`);
 
-    // Welcome message
-    socket.emit('message', `Chat AI :- Welcome to ${room}`)
+		// getting room users.
+		const users = getRoomUsers(user.room);
+		io.to(room).emit("roomUsers", {
+			room: user.room,
+			users,
+		});
+	});
+	socket.on("chatMessage", (msg) => {
+		const user = getCurrentUser(socket.id);
+		io.to(user.room).emit("chatMessage", formateMessage(user.username, msg));
+	});
+	socket.on("typing", (username) => {
+		const user = getCurrentUser(socket.id);
+		socket.broadcast.to(user.room).emit("typing", username);
+	});
+	socket.on("chat", (chat) => {
+		const user = getCurrentUser(socket.id);
+		io.to(user.room).emit("chat", formateMessage(user.username, chat));
+	});
+	socket.on("stream", ({ room, status }) => {
+		const user = getCurrentUser(socket.id);
+		io.to(room).emit("stream", { user, status });
+	});
+	socket.on("voice", function (data) {
+		var newData = data.split(";");
+		newData[0] = "data:audio/ogg;";
+		newData = newData[0] + newData[1];
 
-    // Broadcasting other users
-    socket.broadcast.to(room).emit('message', `Chat AI :- ${username} has joined the room`)
+		for (const id in socketsStatus) {
+			if (id != socketId && !socketsStatus[id].mute && socketsStatus[id].online)
+				socket.broadcast.to(id).emit("send", newData);
+		}
+	});
+	socket.on("userInformation", function (data) {
+		socketsStatus[socketId] = data;
+	});
+	socket.on("disconnect", () => {
+		const user = userLeave(socket.id);
+		console.log("one user left");
+		delete socketsStatus[socketId];
 
-    // getting room users.
-    const users = getRoomUsers(user.room)
-    io.to(room).emit('roomUsers', {
-      room: user.room,
-      users
-    })
-  })
-  socket.on('chatMessage', (msg) => {
-    const user = getCurrentUser(socket.id)
-    io.to(user.room).emit('chatMessage',formateMessage(user.username,msg))
-  })
-  socket.on('typing', (username) => {
-    const user = getCurrentUser(socket.id)
-    socket.broadcast.to(user.room).emit('typing',username)
-  })
-  socket.on('chat', (chat) => {
-    const user = getCurrentUser(socket.id)
-    io.to(user.room).emit('chat',formateMessage(user.username,chat))
-  })
-  socket.on('stream', ({room, status}) => {
-    const user = getCurrentUser(socket.id)
-    io.to(room).emit('stream', {user,status});
-  });
-  socket.on("voice", function (data) {
+		if (user) {
+			// Broadcastion other users on leaving
+			io.to(user.room).emit(
+				"message",
+				`Chat AI :- ${user.username} has left the chat`
+			);
 
-    var newData = data.split(";");
-    newData[0] = "data:audio/ogg;";
-    newData = newData[0] + newData[1];
+			// getting room users.
+			io.to(user.room).emit("roomUsers", {
+				room: user.room,
+				users: getRoomUsers(user.room),
+			});
+		}
+	});
+});
 
-    for (const id in socketsStatus) {
-
-      if (id != socketId && !socketsStatus[id].mute && socketsStatus[id].online)
-        socket.broadcast.to(id).emit("send", newData);
-    }
-
-  });
-  socket.on("userInformation", function (data) {
-    socketsStatus[socketId] = data;
-  });
-  socket.on('disconnect', () => {
-    const user = userLeave(socket.id)
-    console.log('one user left')
-    delete socketsStatus[socketId];
-
-    if(user){
-      // Broadcastion other users on leaving
-      io.to(user.room).emit('message', `Chat AI :- ${user.username} has left the chat`)
-
-      // getting room users.
-      io.to(user.room).emit('roomUsers', {
-        room: user.room,
-        users: getRoomUsers(user.room)
-      })
-    }
-  })
-})
-
-
-server.listen(8080,async () => {
-
-  try {
-
-    await mongoose.connect(process.env.mongoURL)
-    console.log("connected to database..")
-
-  } catch (error) {
-
-    console.log({msg:error.message})
-  }
-  console.log("Listening at 8080");
-})
+server.listen(8080, async () => {
+	try {
+		await mongoose.connect(process.env.mongoURL);
+		console.log("connected to database..");
+	} catch (error) {
+		console.log({ msg: error.message });
+	}
+	console.log("Listening at 8080");
+});
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const { UserModel } = require("../models/user.model");
 const app = express();
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { Blacklist } = require("../models/blacklist.model");
 app.use(express.json());
-require("dotenv").config()
-const session = require('express-session');
-const passport = require('passport');
-const GitHubStrategy = require('passport-github2').Strategy;
+require("dotenv").config();
+const session = require("express-session");
+const passport = require("passport");
+const GitHubStrategy = require("passport-github2").Strategy;
 const userRouter = express.Router();
 
-
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-  }));
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: false,
+	})
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -26,132 +27,132 @@ app.use(passport.session());
 // GITHUB_CALLBACK_URL = http://localhost:8080/auth/github/callback
 // SESSION_SECRET = your_session_secret
 
-passport.use(new GitHubStrategy({
-    clientID: `45f1ae73c281b43ca8a8`,
-    clientSecret: `4fbaf6072fc8ac2166f4f4c095d700fc28ec3dcb`,
-    callbackURL: `http://localhost:8080/auth/github/callback`,
-  }, (accessToken, refreshToken, profile, cb) => {
-    // This function is called when the user is authenticated
-    // You can use the `profile` object to create or update a user in your database
-    return cb(null, profile);
-}));
-
+passport.use(
+	new GitHubStrategy(
+		{
+			clientID: `45f1ae73c281b43ca8a8`,
+			clientSecret: `4fbaf6072fc8ac2166f4f4c095d700fc28ec3dcb`,
+			callbackURL: `http://localhost:8080/auth/github/callback`,
+		},
+		(accessToken, refreshToken, profile, cb) => {
+			// This function is called when the user is authenticated
+			// You can use the `profile` object to create or update a user in your database
+			return cb(null, profile);
+		}
+	)
+);
 
 passport.serializeUser((user, cb) => {
-    cb(null, user);
+	cb(null, user);
 });
-  
+
 passport.deserializeUser((obj, cb) => {
-    cb(null, obj);
+	cb(null, obj);
 });
 
+userRouter.get(
+	"/auth/github",
+	passport.authenticate("github", {
+		scope: ["user:email"],
+	})
+);
 
-userRouter.get('/auth/github', passport.authenticate('github', {
-    scope: ['user:email'],
-}));
+userRouter.get(
+	"/auth/github/callback",
+	passport.authenticate("github", { failureRedirect: "/login" }),
+	(req, res) => {
+		// Successful authentication, redirect to home page.
+		res.redirect("/");
+	}
+);
 
+userRouter.get("/auth/github/callback", (req, res, next) => {
+	passport.authenticate("github", (err, user) => {
+		if (err) {
+			return next(err);
+		}
 
-userRouter.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  (req, res) => {
-    // Successful authentication, redirect to home page.
-    res.redirect('/');
+		req.logIn(user, (err) => {
+			if (err) {
+				return next(err);
+			}
+
+			res.redirect("/");
+		});
+	})(req, res, next);
 });
 
-
-userRouter.get('/auth/github/callback', (req, res, next) => {
-    passport.authenticate('github', (err, user) => {
-      if (err) {
-        return next(err);
-      }
-  
-      req.logIn(user, (err) => {
-        if (err) {
-          return next(err);
-        }
-  
-        res.redirect('/');
-      });
-    })(req, res, next);
-});
-  
-userRouter.get('/', (req, res) => {
-    if (req.isAuthenticated()) {
-      res.send(`Hello, ${req.user.displayName}!`);
-    } else {
-      res.send('Please log in first.');
-    }
+userRouter.get("/", (req, res) => {
+	if (req.isAuthenticated()) {
+		res.send(`Hello, ${req.user.displayName}!`);
+	} else {
+		res.send("Please log in first.");
+	}
 });
 
+userRouter.post("/register", async (req, res) => {
+	try {
+		const { name, email, password } = req.body;
 
+		const isUserExist = await UserModel.findOne({ email });
+		console.log(isUserExist);
+		if (isUserExist) {
+			return res.send({
+				msg: "user already exist in the database try with new email",
+			});
+		}
 
+		const hash = bcrypt.hashSync(password, 8);
+		console.log(hash);
+		const user = new UserModel({ name, email, password: hash });
+		await user.save();
 
+		res.send({ msg: "user has been registered successfully" });
+	} catch (error) {
+		res.send({ msg: error.msg });
+	}
+});
 
-userRouter.post("/register",async (req,res)=>{
+userRouter.post("/login", async (req, res) => {
+	try {
+		const { email, password } = req.body;
+		const isUserExist = await UserModel.findOne({ email });
 
-    try {
-        const {name,email,password} = req.body
+		if (!isUserExist) {
+			return res.status(401).send({ msg: "invalid username or password" });
+		}
 
-        const isUserExist = await UserModel.findOne({email});
-        console.log(isUserExist)
-        if(isUserExist){
-            return res.send({msg:"user already exist in the database try with new email"})
-        }
+		var result = bcrypt.compareSync(password, isUserExist.password);
 
-        const hash = bcrypt.hashSync(password,8)
-        console.log(hash)
-        const user = new UserModel({name,email,password:hash})
-        await user.save()
+		if (!result) {
+			return res.status(401).send({ msg: "invalid username or password" });
+		}
 
-      res.send({msg:"user has been registered successfully"})
-        
-    } catch (error) {
-        res.send({msg:error.msg})
-    }
-})
+		const Accesstoken = jwt.sign(
+			{ userID: isUserExist._id },
+			process.env.accesstoken
+		);
 
-userRouter.post("/login",async (req,res)=>{
+		res.send({ msg: "login successfull", user: isUserExist, Accesstoken });
+	} catch (error) {
+		res.send({ msg: error.msg });
+	}
+});
 
-    try {
-        const {email,password}= req.body
-        const isUserExist = await UserModel.findOne({email})
-       
-        if(!isUserExist){
+userRouter.post("/logout", async (req, res) => {
+	// we wil get the accesstoken and refreshtoken in req.headers with the respective name;
+	try {
+		const accesstoken = req.headers.authorization;
 
-            return res.status(401).send({msg:"invalid username or password"})
-        }
+		const blackAccess = new Blacklist({ token: accesstoken });
+		await blackAccess.save();
 
-        var result=bcrypt.compareSync(password, isUserExist.password)
+		res.send({ msg: "logout successfull....." });
+	} catch (error) {
+		res.send({ msg: error.msg });
+	}
+});
 
-        if(!result){
-            return res.status(401).send({msg:"invalid username or password"})
-        }
-
-        const Accesstoken = jwt.sign({userID:isUserExist._id},process.env.accesstoken)
-
-        res.send({msg:"login successfull",user:isUserExist,Accesstoken})
-    } catch (error) {
-        res.send({msg:error.msg})
-    }
-})
-
-
-userRouter.post("/logout",async (req,res)=>{
-    // we wil get the accesstoken and refreshtoken in req.headers with the respective name;
-        try {
-            const accesstoken = req.headers.authorization
-            
-            const blackAccess = new Blacklist({token:accesstoken});
-            await blackAccess.save()
-    
-            res.send({msg:"logout successfull....."})
-    
-        } catch (error) {
-            res.send({msg:error.msg})
-        }
-    })
-
-
-module.exports={
-    userRouter
-}
+module.exports = {
+	userRouter,
+};
